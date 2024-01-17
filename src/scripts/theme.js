56285a5577c145521d73e8444f191b0969074711
Ju Martins <jumartins958@hotmail.com>
2024-01-17 15:00:52 -0300

    Tarefa 1 concluída

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,24 @@
 /* Desenvolva sua lógica aqui ... */
+
+const html = document.querySelector('html');
+const themeButton = document.querySelector('.header__btn');
+let darkMode = false;
+
+const themeChange = () => {
+    darkMode= !darkMode;
+    html.classList.toggle('dark-mode');
+    themeButton.classList.toggle('header__btn--dark-mode');
+    localStorage.setItem('theme', JSON.stringify(darkMode));
+}
+
+themeButton.addEventListener('click', themeChange);
+
+const themeAnalysis = () => {
+    darkMode = JSON.parse(localStorage.getItem('theme'));
+    if(darkMode){
+        html.classList.add('dark-mode');
+        themeButton.classList.add('header__btn--dark-mode');
+    }
+}
+
+themeAnalysis();
